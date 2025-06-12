14fa95ba811997e71e2e7b36351c108ca1191fa7
JacksonGuy <jacksonf955@gmail.com>
2025-06-12 00:55:05 -0400

    fuck we have to change everything

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use std::sync::{
         channel,
     },
 };
+use std::net::{UdpSocket};
 
 use serde_json::json;
 
@@ -66,6 +67,19 @@ async fn download(items: Vec<String>) {
                 .expect("Failed to write to file");
         }
         */
+    }
+
+    // Create UDP Socket
+    let socket = UdpSocket::bind("127.0.0.1:8080")
+        .expect("Failed to bind socket");
+
+    loop {
+        let mut buf: Vec<u8> = Vec::new();
+        socket.recv(&mut buf).unwrap();
+
+        let data = String::from_utf8(buf).unwrap();
+        let packet: Packet = serde_json::from_str(data.as_str())
+            .expect("Failed to deserialize packet");
     }
 }
 
